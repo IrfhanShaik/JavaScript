3288adc0b2614a0829a231be33026c324ea6e7d4
Irfan Shaik <shaikirfan2349@gmail.com>
2025-04-02 10:00:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  // Treat all JS code as Newer Version
+
+// alert(3+3) give error we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+let name = "Irfan"
+let age = 18
+let isLoggedIn = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+console.log(undefined)
+console.log(null)  //mcq question null type is object
